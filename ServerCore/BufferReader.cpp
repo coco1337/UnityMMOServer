3d1651cd044ced61246e96d169952554894a64a9
coco1337 <scar3517@gmail.com>
2022-07-07 11:10:02 +0900

    Add utils, DB codes

---
+++ ServerCore/BufferReader.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "BufferReader.h"
+
+BufferReader::BufferReader()
+{
+}
+
+BufferReader::~BufferReader()
+{
+}
+
+bool BufferReader::Peek(void* dest, uint32 len)
+{
+	if (FreeSize() < len)
+		return false;
+
+	::memcpy(dest, &_buffer[_pos], len);
+	return true;
+}
+
+bool BufferReader::Read(void* dest, uint32 len)
+{
+	if (Peek(dest, len) == false) 
+		return false;
+	
+	_pos += len;
+	return true;
+}
